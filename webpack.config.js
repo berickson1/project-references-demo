5f271d133e05cd2850cced5788207483b44e8893
Brent Erickson <berickson1@users.noreply.github.com>
2020-04-13 10:11:37 -0700

    Update to use ts-loader

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+"use strict";
+const path = require("path");
+
+module.exports = {
+  devtool: "inline-source-map",
+  entry: "./zoo/zoo.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          logLevel: "info",
+          logInfoToStdOut: true,
+          projectReferences: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+};
